0d1675422a1d72c140c70d19f4c22c45bf8d4f9f
AYUSH KUMAR <ayush.kr.26.04@gmail.com>
2022-04-13 10:31:52 +0530

    Chat App

---
+++ client.js
@@ -0,0 +1,32 @@
+const socket = io('http://localhost:8000');
+const form = document.getElementById('sendContainer');
+const messageInput = document.getElementById('messageInput');
+const messageContainer = document.querySelector('.container');
+var audio = new Audio('http://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3');
+const append = (message,position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if (position=='left') audio.play();
+}
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`,'right');
+    socket.emit('send',message);
+    messageInput.value = "";
+})
+const name = prompt("Enter your name to join the chat:"); 
+console.log(name);
+socket.emit('new-user-joined',name);
+socket.on('user-joined',name=>{
+    append(`${name} joined the chat`,'right');
+})
+socket.on('receive',data=>{
+    append(`${data.name}:${data.message}`,'left');
+})
+socket.on('leave',name=>{
+    append(`${name} left the chat`,'left');
+})
